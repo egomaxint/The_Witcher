8e0276914bd1c7ef9dbecc265ae291820192794f
Nikitenko Max <nikitenkomax70@gmail.com>
2020-10-27 09:55:04 +0300

    add script, changed html, css

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
-'use strict'
+'use strict';
+
+const menuButton = document.querySelector('.menu-button');
+const header = document.querySelector('.header');
+
+menuButton.addEventListener('click', function () {
+  header.classList.toggle('header-active');
+});
 
 var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
-    loop: true,
-    navigation: {
+  slidesPerView: 2,
+  loop: true,
+  navigation: {
     nextEl: '.arrow',
-    
   },
 });
