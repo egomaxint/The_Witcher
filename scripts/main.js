d9f9d16278dc71aae3b24bb858dfcbf0d9bbe227
Nikitenko Max <nikitenkomax70@gmail.com>
2020-11-06 14:22:32 +0300

    last version

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,30 @@
 
 const menuButton = document.querySelector('.menu-button');
 const header = document.querySelector('.header');
+const star = document.querySelectorAll('.star');
+
+let count = 0;
+star.forEach((e) => {
+  e.style.animationDelay = `${(count += 0.1)}s`;
+});
 
 menuButton.addEventListener('click', function () {
   header.classList.toggle('header-active');
+  menuButton.classList.toggle('menu-button-active');
 });
 
 var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: '.arrow',
+  },
+  breakpoints: {
+    541: {
+      slidesPerView: 2,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
   },
 });
